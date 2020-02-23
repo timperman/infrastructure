ae5e413065337bb727b1b0f785ab0ece9f17ed33
Brett Timperman <brett.timperman@gmail.com>
2020-02-23 17:32:49 -0500

    sudo install docker on debian

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -6,11 +6,11 @@ apt-get install -y git \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
+    gnupg2 \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 apt-get update
