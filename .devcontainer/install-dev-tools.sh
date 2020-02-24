91a486b294b32e5641134896105ae3b7e6b8ee49
Brett Timperman <brett.timperman@gmail.com>
2020-02-23 19:08:32 -0500

    install docker with script

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
 # install docker
-apt-get update
-apt-get install -y git \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com | sudo sh -
 
 # install kubectl
 curl -sSL -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 
 chmod +x /usr/local/bin/kubectl 
 
 # install helm
-curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash -
+curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get | sudo bash -
 
 # install doctl
 curl -sL https://github.com/digitalocean/doctl/releases/download/v1.38.0/doctl-1.38.0-linux-amd64.tar.gz | tar -xzv
